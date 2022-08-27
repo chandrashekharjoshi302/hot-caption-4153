6be69c168917a794992c3b1d9ad489e589ccca5a
chandrashekharjoshi302 <chandrashekharjoshi02@gmail.com>
2022-08-28 00:02:11 +0530

    add

---
+++ components/navbar.js
@@ -0,0 +1,44 @@
+
+
+function navbar(){
+    return ` 
+    <div id="nav">
+   
+    <div> <a href="index.html"> <img src="https://elearningindustry.com/wp-content/uploads/2021/03/37af1f218319cf521d01ac44ad330a6f.png" alt=""></a> </div>
+  
+    <div id="nav2">
+    
+        <div id="nav3">
+            <div class="show">Integrations
+                <div id="int" class="hide">
+                    <div id="grid">
+                        <div><img src="/images/asana.png" alt=""><a href="/asana.html">Asana</a></div>
+                        <div><img src="/images/clickup.png" alt=""><a href="#">Clickup</a></div>
+                        <div><img src="/images/basecamp.png" alt=""><a href="/basecamp.html">Basecamp</a></div>
+                        <div><img src="/images/trello.png" alt=""><a href="/Trello.html">Trello</a></div>
+                        <div><img src="/images/jira.png" alt=""><a href="/Jira.html">Jira</a></div>
+                        <div><img src="/images/monday.png" alt=""><a href="#">Monday</a></div>
+                        <div><img src="/images/github.png" alt=""><a href="/GitHub.html">Github</a></div>
+                        <div><img src="/images/gitlab.png" alt=""><a href="/GitLab.html">GitLab</a></div>
+                        <div><img src="/images/todoist.png" alt=""><a href="/todoist-time-tracking.html">Todoist</a></div>
+                        <div><img src="/images/Notion.png" alt=""><a  href="#">Notion</a></div>
+                        <div><img src="/images/xero.png" alt=""><a href="#">Xero</a></div>
+                        <div><img src="/images/Quickbook.png" alt=""><a href="/quickbook.html">QuickBooks</a></div>
+                    </div>
+                    <div id="inte">Everhour offers real integration with your favorite tools.
+                        <a href="/integrations.html">All integrations</a>
+                    </div>
+                </div>
+            </div>
+            <div><a href="/Pricing.html">Pricing </a></div>
+            <div><a href="/demo.html">Demo</a></div>
+        </div>
+        <div id="nav4">
+            <span><a href="/login.html">Log In</a></span>
+            <button  ><a href="/signup.html">Sign up</a></button>
+        </div>
+</div>
+</div> `
+}
+
+export default navbar;
---
+++ components_2/footer.js
@@ -0,0 +1,90 @@
+function footer(){
+    return ` <div id="main_c">
+    <div id="inner_part">
+        <img src="/images/footer_logo.png" alt="">
+    <div class="inner_col">
+        <h3>Company</h3>
+        <br>
+        <p><a href="https://everhour.com/about">about us</a></p>
+            <p><a href="https://mail.google.com/mail/u/0/#inbox">Contact</a></p>
+        
+    </div>
+    <div class="inner_col">
+        <h3>Product</h3>
+        <br>
+        <p><a href="https://everhour.com/tour">Tour</a></p>
+        <p><a href="https://everhour.com/pricing">Pricing</a></p>
+        <p><a href="https://everhour.com/request-a-demo">Reqest a demo</a></p>
+        <p><a href="https://everhour.com/customers">Customers</a></p>
+        <p><a href="https://everhour.docs.apiary.io/#">API & does </a></p>
+        <p><a href="https://status.everhour.com/">Status </a></p>
+    </div>
+    <div class="inner_col"><h3>Solutions</h3>
+    <br>
+        <p><a href="https://everhour.com/time-tracking">Time tracking</a></p>
+        <p><a href="https://everhour.com/project-budgeting">Project budgeting</a></p>
+        <p><a href="https://everhour.com/resource-planning">Visual planning</a></p>
+        <p><a href="https://everhour.com/expenses">Expenses</a></p>
+        <p><a href="https://everhour.com/reporting">Reporting</a></p>
+        <p><a href="https://everhour.com/invoicing">Invoicing</a></p>
+        <p><a href="https://everhour.com/free-time-card-calculator">Time Card Calculator</a></p>
+        <p><a href="https://everhour.com/free-project-timesheet">Weekly timesheet Template</a></p>
+        <p><a href="https://everhour.com/free-invoice-generator">Invoicegenerator</a></p>
+    </div>
+    <div class="inner_col"><h3>Integrations</h3>
+    <br>
+        <p><a href="https://everhour.com/integrations/asana">Asana</a></p>
+            <p><a href="https://everhour.com/integrations/basecamp">Basecamp</a></p>
+            <p><a href="https://everhour.com/integrations/trello">Trello</a></p>
+            <p><a href="https://everhour.com/integrations/jira">Jira</a></p>
+            <p><a href="https://everhour.com/integrations/github">Github</a></p>
+            <p><a href="https://everhour.com/integrations/clickup">Clickup</a></p>
+            <p><a href="https://everhour.com/integrations/monday">Monday</a></p>
+            <p><a href="https://everhour.com/integrations/notion-time-tracking">Notion</a></p>
+            <p><a href="https://everhour.com/integrations/todoist-time-tracking">Todoist</a></p>
+            <p><a href="https://everhour.com/integrations/gitlab-time-tracking">GitLab</a></p>
+            <p><a href="https://everhour.com/integrations">All</a></p>
+    
+    </div>
+    <div class="inner_col"><h3>Learn</h3>
+    <br>
+        <p><a href="https://everhour.com/resources">Resources</a></p>
+        <p><a href="https://support.everhour.com/">Help Center</a></p>
+        <p><a href="https://everhour.com/blog/">Blog</a></p>
+        <p><a href="https://everhour.com/blog/category/product-updates/">Product updates</a></p>
+        <p><a href="https://everhour.com/timesheet-app">Timesheet app</a></p>
+        <p><a href="https://everhour.com/time-clock-app">Time click app</a></p>
+        <p><a href="https://everhour.com/attendance-tracker">Attendance Tracker</a></p>
+        <p><a href="https://everhour.com/work-hours-tracker">Work hours Tracker</a></p>
+        <p><a href="https://everhour.com/employee-time-tracking">Employee time tracking</a></p>
+    </div>
+    <div class="inner_col"><h3>Download</h3>
+    <br>
+        <p><a href="https://everhour.com/extensions">Browser extension</a></p>
+            <p><a href="https://everhour.com/chrome-time-tracking">Chrome time tracking extension</a></p>
+            <p><a href="https://everhour.com/time-tracking-with-screenshots">Privacy</a></p>
+            <p><a href="https://apps.apple.com/us/app/everhour/id1372059771">Time tracking with Screenshots</a></p>
+    
+    </div>
+
+    </div>
+    <div id="last_part" >
+        <div>
+            <h1>© 2022 Everhour Terms| Privacy| Cookies | Sitemap</h1>
+        </div>
+        <div>
+            <span><a href="https://www.facebook.com/timecamp/"><img src="https://cdn-m.timecamp.com/img/greenbranding/facebook.svg"/></a></span>
+            <span><a href="https://www.linkedin.com/company/timecamp/"><img src="https://cdn-m.timecamp.com/img/greenbranding/linkedin.svg"alt=""></a></span>
+            <span><a href="https://twitter.com/timecamp/"><img src="https://cdn-m.timecamp.com/img/greenbranding/twitter.svg" alt=""></a></span>
+            <span><i class="fa fa-youtube-play" style="font-size:20px;color:red"></i></span>
+
+        </div>
+       
+    </div>
+
+</div>
+
+    `
+}
+
+export default footer
